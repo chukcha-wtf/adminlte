a0cec382613a8f64d84262cf1c515f8182225281
Pavlo Babenko <pavlo.babenko@gmail.com>
2015-01-10 18:39:22 +0200

    Adding precompiling assets

--- lib/adminlte.rb
+++ lib/adminlte.rb
@@ -2,5 +2,12 @@ require "adminlte/version"
 
 module Adminlte
   class Engine < ::Rails::Engine
+      initializer 'adminlte.assets.precompile' do |app|
+        %w(stylesheets javascripts fonts img).each do |sub|
+          app.config.assets.paths << root.join('vendor', 'assets', sub)
+        end
+
+        app.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+      end
   end
 end
